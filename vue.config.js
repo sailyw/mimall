ff349fec9c6da1dbd601760bb752ad39b9b466b2
sailyw <1152827086@qq.com>
2020-03-16 17:18:58 +0800

    add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+// 代理
+module.exports ={
+    devServer:{
+        host:'localhost',
+        port:8080,
+        proxy:{
+            '/api':{
+                target:'http://mall-pre.springboot.cn',
+                changeOrigin:true,
+                pathRewrite:{
+                    '/api':''
+                }
+            }
+        }
+    }
+}
